5e50f17dd59ae5121822178d2c58432564e71381
Liliana Gorga <gorgaliliana87@gmail.com>
2024-06-08 14:58:10 +0200

    trigger deploy

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,7 +11,11 @@ class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request): Response
     {
-        $user = User::create($request->validated());
+//        $user = User::create($request->validated());
+        $user = User::create(array_merge(
+            $request->validated(),
+            ['email_verified_at' => now()]
+        ));
         return response($user, Response::HTTP_CREATED);
     }
 }
